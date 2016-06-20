38e3e38debbdf6c9663d648759b29f2b9f3b259a
2masswag <thomassonneveldt@planet.nl>
2016-06-20 21:43:54 +0200

    Update databaseScript.sql

--- databaseScript.sql
+++ databaseScript.sql
@@ -1,59 +1,60 @@
 CREATE TABLE IF NOT EXISTS Eigenaren
 ( 
-	EigenaarID SMALLINT NOT NULL,
-	Naam_Eigenaar CHAR(24) NOT NULL  ,
-	Email_Eigenaar CHAR(30) NOT NULL ,
-	Telefoonnummer_Eigenaar CHAR(15) NOT NULL ,
-	PRIMARY KEY(EigenaarID)
+EigenaarID SMALLINT NOT NULL AUTO_INCREMENT,
+Naam_Eigenaar CHAR(24) NOT NULL  ,
+Email_Eigenaar CHAR(30) NOT NULL ,
+Telefoonnummer_Eigenaar CHAR(15) NOT NULL ,
+PRIMARY KEY(EigenaarID)
 ) ;
 
 CREATE TABLE IF NOT EXISTS Producten
 (
-	ProductID SMALLINT NOT NULL ,
-	Prijs CHAR(12) NOT NULL ,
-	EigenaarID SMALLINT NOT NULL ,
-	Model_Auto CHAR(25) NOT NULL ,
-	Vermogen CHAR(8) NOT NULL ,
-	Zitplaatsen CHAR(2) NOT NULL ,
-	Deuren CHAR(2) NOT NULL ,
-	Verbruik CHAR(10) NOT NULL ,
-	Brandstof CHAR(12) NOT NULL ,
-	Aandrijving CHAR(15) NOT NULL ,
-	Transmissie CHAR(15) NOT NULL ,
-	Aantal_Versnellingen CHAR(3) NOT NULL ,
-	Kilometerstand CHAR(15) NOT NULL ,
-	PRIMARY KEY(ProductID) ,
-	FOREIGN KEY (EigenaarID)
-	REFERENCES Eigenaren(EigenaarID) 
+ProductID SMALLINT NOT NULL AUTO_INCREMENT,
+Prijs CHAR(12) NOT NULL ,
+EigenaarID SMALLINT NOT NULL ,
+MERK_AUTO CHAR(25) NOT NULL ,
+Model_Auto CHAR(25) NOT NULL ,
+BOUWJAAR CHAR (5) NOT NULL ,
+Vermogen CHAR(8) NOT NULL ,
+Zitplaatsen CHAR(2) NOT NULL ,
+Deuren CHAR(2) NOT NULL ,
+Verbruik CHAR(10) NOT NULL ,
+Brandstof CHAR(12) NOT NULL ,
+Aandrijving CHAR(15) NOT NULL ,
+Transmissie CHAR(15) NOT NULL ,
+Aantal_Versnellingen CHAR(3) NOT NULL ,
+Kilometerstand CHAR(15) NOT NULL ,
+PRIMARY KEY(ProductID) ,
+FOREIGN KEY (EigenaarID)
+REFERENCES Eigenaren(EigenaarID) 
 ) ;
 
 CREATE TABLE IF NOT EXISTS Klanten
 ( 
-	KlantID SMALLINT NOT NULL,
-	Voornaam CHAR(24) NOT NULL,
-	Tussenvoegsel CHAR(12),
-	Achternaam Char(24) NOT NULL,
-	Email CHAR(30) NOT NULL ,
-	Telefoonnummer CHAR(15) NOT NULL ,
-	Straat CHAR(20) NOT NULL ,
-	Woonplaats CHAR(20) NOT NULL ,
-	Postcode CHAR (7) NOT NULL ,
-	Bestelhistorie CHAR(50) NOT NULL ,
-	PRIMARY KEY (KlantID) 
+KlantID SMALLINT NOT NULL AUTO_INCREMENT,
+Voornaam CHAR(24) NOT NULL,
+Tussenvoegsel CHAR(12),
+Achternaam Char(24) NOT NULL,
+Email CHAR(60) NOT NULL ,
+Telefoonnummer CHAR(15) NOT NULL ,
+Straat CHAR(20) NOT NULL ,
+Woonplaats CHAR(20) NOT NULL ,
+Postcode CHAR (7) NOT NULL ,
+PRIMARY KEY (KlantID) 
 ) ;
 
 CREATE TABLE IF NOT EXISTS Bestellingen
 (
-	BestellingID SMALLINT NOT NULL , 
-	KlantID SMALLINT NOT NULL ,
-	ProductID SMALLINT NOT NULL ,
-	Prijs CHAR(12) NOT NULL ,
-	EigenaarID SMALLINT NOT NULL ,
-	PRIMARY KEY (BestellingID) ,
-	FOREIGN KEY (KlantID) 
-	REFERENCES Klanten(KlantID) ,
-	FOREIGN KEY (ProductID) 
-	REFERENCES Producten(ProductID) ,
-	FOREIGN KEY (EigenaarID)
-	REFERENCES Eigenaren(EigenaarID)
+BestellingID SMALLINT NOT NULL AUTO_INCREMENT , 
+KlantID SMALLINT NOT NULL ,
+ProductID SMALLINT NOT NULL ,
+Prijs CHAR(12) NOT NULL ,
+EigenaarID SMALLINT NOT NULL ,
+PRIMARY KEY (BestellingID) ,
+FOREIGN KEY (KlantID) 
+REFERENCES Klanten(KlantID) ,
+FOREIGN KEY (ProductID) 
+REFERENCES Producten(ProductID) ,
+FOREIGN KEY (EigenaarID)
+REFERENCES Eigenaren(EigenaarID)
 ) ;
